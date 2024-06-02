d4f29adae1a0287d99aa06b5ff4cde482ee33b6c
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-06-02 21:17:31 +0800

    change model

--- app/src/main/java/com/andi/driver/behavior/utils/Constants.kt
+++ app/src/main/java/com/andi/driver/behavior/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.andi.driver.behavior.utils
 
 object Constants {
-    const val MODEL_PATH = "model.tflite"
+    const val MODEL_PATH = "best.tflite"
     const val LABELS_PATH = "labels.txt"
 }
